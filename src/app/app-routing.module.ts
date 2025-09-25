1ab8ade8e8ff17efedc593c4d690c96e57919e6f
Neiconi Andrei <a.neiconi19@gmail.com>
2025-09-26 02:47:34 +0300

    initial commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,24 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { AboutComponent } from './pages/about/about.component';
+import { SkillsComponent } from './pages/skills/skills.component';
+import { ProjeComponent } from './pages/proje/proje.component';
+import { ContactComponent } from './pages/contact/contact.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'about', component: AboutComponent },
+  { path: 'skills', component: SkillsComponent },
+  { path: 'projects', component: ProjeComponent },
+  { path: 'contact', component: ContactComponent },
+  { path: '**', redirectTo: '' },
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  imports: [
+    RouterModule.forRoot(routes, { scrollPositionRestoration: 'enabled' }),
+  ],
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
---
+++ src/app/pages/proje/proje.component.ts
@@ -0,0 +1,26 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-proje',
+  templateUrl: './proje.component.html',
+  styleUrls: ['./proje.component.scss'],
+})
+export class ProjeComponent {
+  projects = [
+    {
+      title: 'Sezonul Dragostei',
+      description:
+        'A dating web service that provide guidence in finding the love of your life',
+      image: 'assets/project-1.png',
+      demo: 'https://serviciidematrimoniale.ro/',
+      github: 'https://github.com/AndreiNeiconi/SezonulDragostei.git',
+    },
+    {
+      title: 'Bio natur dayneo market',
+      description: 'A marketplace for seling natural juice ',
+      image: 'assets/project2.png',
+      demo: 'https://bio-natur-market.netlify.app',
+      github: '#',
+    },
+  ];
+}
